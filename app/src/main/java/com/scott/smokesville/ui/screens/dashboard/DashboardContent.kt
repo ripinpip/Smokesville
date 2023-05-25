49695bf7adbc07c08b5af4bb9ca88f759bcd4083
ripinpip <thanebhickman@gmail.com>
2023-05-24 20:25:35 -0600

    Set Target Display UI

--- app/src/main/java/com/scott/smokesville/ui/screens/dashboard/DashboardContent.kt
+++ app/src/main/java/com/scott/smokesville/ui/screens/dashboard/DashboardContent.kt
@@ -3,12 +3,15 @@ package com.scott.smokesville.ui.screens.dashboard
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.scott.smokesville.R
 import com.scott.smokesville.ui.theme.appBackgroundColor
 import com.scott.smokesville.ui.theme.appContentColor
@@ -33,36 +36,53 @@ fun DisplayTemperature() {
                 .fillMaxWidth()
         ) {
             Row(
-                modifier = Modifier.fillMaxWidth(),
+                modifier = Modifier.fillMaxWidth()
+                    .height(IntrinsicSize.Min),
                 horizontalArrangement = Arrangement.Center,
             ) {
-                IconButton(
-                    onClick = {
-
-                    }
+                Box(
+                    modifier = Modifier.weight(1f)
+                        .fillMaxHeight(),
+                    contentAlignment = Alignment.Center
                 ) {
-                    Icon(
-                        painter = painterResource(id = R.drawable.ic_subtract),
-                        contentDescription = stringResource(id = R.string.subtract),
-                        tint = MaterialTheme.colors.appContentColor
-                    )
+                    IconButton(
+                        onClick = {
+
+                        }
+                    ) {
+                        Icon(
+                            modifier = Modifier.size(60.dp),
+                            painter = painterResource(id = R.drawable.ic_subtract),
+                            contentDescription = stringResource(id = R.string.subtract),
+                            tint = MaterialTheme.colors.appContentColor
+                        )
+                    }
                 }
                 Text(
-                    text = "74",
+                    modifier = Modifier.weight(2f),
+                    text = "250",
                     color = MaterialTheme.colors.appContentColor,
                     style = MaterialTheme.typography.h1,
-                    fontWeight = FontWeight.Bold
+                    fontWeight = FontWeight.Bold,
+                    textAlign = TextAlign.Center
                 )
-                IconButton(
-                    onClick = {
-
-                    }
+                Box(
+                    modifier = Modifier.weight(1f)
+                        .fillMaxHeight(),
+                    contentAlignment = Alignment.Center
                 ) {
-                    Icon(
-                        painter = painterResource(id = R.drawable.ic_add),
-                        contentDescription = stringResource(id = R.string.subtract),
-                        tint = MaterialTheme.colors.appContentColor
-                    )
+                    IconButton(
+                        onClick = {
+
+                        }
+                    ) {
+                        Icon(
+                            modifier = Modifier.size(60.dp),
+                            painter = painterResource(id = R.drawable.ic_add),
+                            contentDescription = stringResource(id = R.string.subtract),
+                            tint = MaterialTheme.colors.appContentColor
+                        )
+                    }
                 }
             }
         }
